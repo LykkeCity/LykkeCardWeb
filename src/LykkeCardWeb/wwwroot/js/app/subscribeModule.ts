c879cb6a4308e83ee01cb3c65993674601f671f4
Dmitry Samodov <samodov.dmitry@gmail.com>
2017-12-11 19:32:18 +0300

    added limits info

---
+++ src/LykkeCardWeb/wwwroot/js/app/subscribeModule.ts
@@ -0,0 +1,12 @@
+﻿/// <reference path="../typings/globals/angular/index.d.ts" />
+/// <reference path="../typings/globals/jquery/index.d.ts" />
+
+module CardModule {
+    angular.module('cardApp', []).run($http => {
+        $http.defaults.headers.common['RequestVerificationToken'] = angular.element('input[name="__RequestVerificationToken"]').attr('value');
+        $('.ng-hidden').removeClass('ng-hidden');
+    });
+
+    angular.module('cardApp').service('ISubscribeService', SubscribeService);
+    angular.module('cardApp').controller('SubscribeController', SubscribeController);
+}
